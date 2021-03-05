9740098660d274ccfb86ae45ccfb049c5c64ef63
Dani Renner <danijrenner@gmail.com>
2021-03-05 09:44:47 -0800

    constructors tests PASS, add clearAll()

---
+++ VendorTracker/Models/Order.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+namespace VendorTracker.Models
+{
+  public class Order
+  {
+    public string OrderDetails { get; set;}
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> {};
+    public Order(string orderDetails)
+    {
+      OrderDetails = orderDetails;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+        public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
